06a3d98b349d9c1e4b5360109f7d836db8a67b0f
Arthur Halbart <arthur.halbart@gmail.com>
2025-06-05 17:07:50 +0200

    Image_page_home

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,19 @@
-console.info('Hello world');
+const canvas = document.getElementById('noiseCanvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  function generateNoise() {
+  const imageData = ctx.createImageData(canvas.width, canvas.height);
+  const buffer = new Uint32Array(imageData.data.buffer);
+  for (let i = 0; i < buffer.length; i++) {
+    buffer[i] = (255 << 24) | // alpha
+                (Math.random() * 255 << 16) | // red
+                (Math.random() * 255 << 8) | // green
+                (Math.random() * 255); // blue
+  }
+  ctx.putImageData(imageData, 0, 0);
+  requestAnimationFrame(generateNoise);
+}
+
+generateNoise();
